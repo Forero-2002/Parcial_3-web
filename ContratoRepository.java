d7fb2a795f9d40a4ef209060f7ea62ddf8d1598c
Juan Sebastian Forero Moreno <juans_forero@javeriana.edu.co>
2025-05-27 16:36:17 -0500

    Create ContratoRepository.java

---
+++ ContratoRepository.java
@@ -0,0 +1,7 @@
+package com.juanforero.parcial.repository;
+
+import com.juanforero.parcial.entity.Contrato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContratoRepository extends JpaRepository<Contrato, Long> {
+}
