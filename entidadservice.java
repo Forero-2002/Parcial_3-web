71260a7cdb0aea0d3b61249a55bb035ae9dc9023
Juan Sebastian Forero Moreno <juans_forero@javeriana.edu.co>
2025-05-27 16:36:44 -0500

    Create entidadservice.java

---
+++ entidadservice.java
@@ -0,0 +1,49 @@
+package com.juanforero.parcial.service;
+
+import com.juanforero.parcial.dto.EntidadDTO;
+import com.juanforero.parcial.entity.Entidad;
+import com.juanforero.parcial.repository.EntidadRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EntidadService {
+    
+    @Autowired
+    private EntidadRepository entidadRepository;
+    
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public EntidadDTO createEntidad(EntidadDTO entidadDTO) {
+        Entidad entidad = modelMapper.map(entidadDTO, Entidad.class);
+        Entidad savedEntidad = entidadRepository.save(entidad);
+        return modelMapper.map(savedEntidad, EntidadDTO.class);
+    }
+    
+    public EntidadDTO updateEntidad(Long id, EntidadDTO entidadDTO) {
+        Entidad entidad = entidadRepository.findById(id).orElseThrow();
+        modelMapper.map(entidadDTO, entidad);
+        Entidad updatedEntidad = entidadRepository.save(entidad);
+        return modelMapper.map(updatedEntidad, EntidadDTO.class);
+    }
+    
+    public void deleteEntidad(Long id) {
+        entidadRepository.deleteById(id);
+    }
+    
+    public List<EntidadDTO> getAllEntidades() {
+        return entidadRepository.findAll().stream()
+                .map(entidad -> modelMapper.map(entidad, EntidadDTO.class))
+                .collect(Collectors.toList());
+    }
+    
+    public EntidadDTO getEntidadById(Long id) {
+        Entidad entidad = entidadRepository.findById(id).orElseThrow();
+        return modelMapper.map(entidad, EntidadDTO.class);
+    }
+}
